5199de5fd2feb5e906d9ddf7b2af2e7d9433d983
Jay <1147972163@qq.com>
2019-02-04 12:13:22 +0800

    Upd #2

--- ECalc/Parse_unit.h
+++ ECalc/Parse_unit.h
@@ -3,9 +3,27 @@ class Parse_unit
 {
 private:
 	short type;//0=num,1=+,2=-£¬3=*£¬4=/
-	
+	int num;
 public:
 	Parse_unit()=default;
+	Parse_unit(int number) :num(number) {};
+	Parse_unit(char ch) {
+		switch (ch)
+		{
+		case '+':
+			this->type = 1;
+			break;
+		case '-':
+			this->type = 2;
+			break;
+		case '*':
+			this->type = 3;
+			break;
+		case '/':
+			this->type = 4;
+			break;
+		}
+	}
 	~Parse_unit()=default;
 };
 
