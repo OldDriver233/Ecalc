e2bcb3c851b8aba8a63c6637badbcf704152598e
Jay <1147972163@qq.com>
2019-02-04 12:20:54 +0800

    Upd #3

--- ECalc/ECalc.cpp
+++ ECalc/ECalc.cpp
@@ -1,7 +1,18 @@
 ﻿#include"pch.h"
 #include<iostream>
 #include<string>
+#include<vector>
+#include<stack>
+#include"Parse_unit.h"
 using namespace std;
+
+vector<Parse_unit>vec;
+void reader()
+{
+	string strin;
+	vec.clear();
+	getline(cin, strin);
+}
 
 int main()
 {
